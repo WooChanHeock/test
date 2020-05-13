f537f1cdcc6d8f6932427f2d2281524ed2f9b7c3
WooChanHeock <63646625+WooChanHeock@users.noreply.github.com>
2020-05-14 04:22:45 +0900

    rep8 0514

--- MyProject/src/main/java/kr/inhatc/spring/board/controller/BoardController.java
+++ MyProject/src/main/java/kr/inhatc/spring/board/controller/BoardController.java
@@ -2,13 +2,17 @@ package kr.inhatc.spring.board.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
+
 
 import kr.inhatc.spring.board.dto.BoardDto;
 import kr.inhatc.spring.board.service.BoardService;
@@ -16,6 +20,8 @@ import kr.inhatc.spring.board.service.BoardService;
 @Controller
 public class BoardController {
 
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private BoardService boardService;
 	
@@ -27,6 +33,7 @@ public class BoardController {
 	@RequestMapping("/board/boardList")
 	public String boardList(Model model) {
 		List<BoardDto> list = boardService.boardList();
+		log.debug("================>" + list.size());
 		//System.out.println("================>" + list.size());
 		model.addAttribute("list", list);
 		return "board/boardList";
@@ -38,8 +45,8 @@ public class BoardController {
 	}
 	
 	@RequestMapping("/board/boardInsert")
-	public String boardInsert(BoardDto board) {
-		boardService.boardInsert(board);
+	public String boardInsert(BoardDto board, MultipartHttpServletRequest multipartHttpServletRequest) {
+		boardService.boardInsert(board, multipartHttpServletRequest);
 		return "redirect:/board/boardList";
 	}
 	
--- MyProject/src/main/java/kr/inhatc/spring/board/service/BoardServiceimpl.java
+++ MyProject/src/main/java/kr/inhatc/spring/board/service/BoardServiceimpl.java
@@ -1,18 +1,28 @@
 package kr.inhatc.spring.board.service;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import kr.inhatc.spring.board.dto.BoardDto;
+import kr.inhatc.spring.board.dto.FileDto;
 import kr.inhatc.spring.board.mapper.BoardMapper;
+import kr.inhatc.spring.utils.FileUtils;
 
 @Service
 public class BoardServiceimpl implements BoardService {
 
 	@Autowired
 	private BoardMapper boardMapper;
+	
+	@Autowired
+	private FileUtils fileUtils;
 	
 	@Override
 	public List<BoardDto> boardList() {
@@ -21,8 +31,30 @@ public class BoardServiceimpl implements BoardService {
 	}
 
 	@Override
-	public void boardInsert(BoardDto board) {
+	public void boardInsert(BoardDto board, MultipartHttpServletRequest multipartHttpServletRequest) {
 		boardMapper.boardInsert(board);
+		
+		//임시 확인
+//		if(ObjectUtils.isEmpty(multipartHttpServletRequest) == false){
+//			Iterator<String> iter = multipartHttpServletRequest.getFileNames();
+//			while(iter.hasNext()) {
+//				String name = iter.next();
+//				
+//				List<MultipartFile> list = multipartHttpServletRequest.getFiles(name);
+//				for (MultipartFile multipartFile : list) {
+//					System.out.println("++++++++++++++++++++++++++++++++>file name" + multipartFile.getOriginalFilename());
+//					System.out.println("++++++++++++++++++++++++++++++++>file size" + multipartFile.getSize());
+//					System.out.println("++++++++++++++++++++++++++++++++>file type" + multipartFile.getContentType());
+//				}
+//			}
+//		}
+		//1.파일 저장
+		List<FileDto> list = fileUtils.parseFileInfo(board.getBoardIdx(), multipartHttpServletRequest); 
+		
+		//2.디비 저장
+		if(CollectionUtils.isEmpty(list) == false) {
+			boardMapper.boardFileInsert(list);
+		}
 		
 	}
 
